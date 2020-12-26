a0c23457fe87b9f06c01d3a83316eb381dc67399
Paul Horn <developer@knutwalker.de>
2020-12-26 14:39:47 +0100

    Improve Day 23 solution

--- src/day23.rs
+++ src/day23.rs
@@ -12,11 +12,11 @@ register!(
 fn run1(input: &[u8], games: u32) -> Output {
     let cups = run_any(input, 10, games);
 
-    let mut cup = cups[1].next;
+    let mut cup = cups[1];
     let mut result = String::new();
     while cup != 1 {
         result.push((b'0' + cup as u8) as char);
-        cup = cups[cup as usize].next;
+        cup = cups[cup as usize];
     }
 
     result.parse().unwrap()
@@ -25,21 +25,14 @@ fn run1(input: &[u8], games: u32) -> Output {
 fn run2(input: &[u8]) -> Output {
     let cups = run_any(input, 1_000_001, 10_000_000);
 
-    let start = cups[1];
-    let c1 = cups[start.next as usize];
-    let c2 = cups[c1.next as usize];
-    u64::from(c1.value) * u64::from(c2.value)
+    let c1 = cups[1];
+    let c2 = cups[c1 as usize];
+    u64::from(c1) * u64::from(c2)
 }
 
-#[derive(Debug, Copy, Clone)]
-struct Cup {
-    value: u32,
-    next: u32,
-}
-
-fn run_any(input: &[u8], cards: u32, games: u32) -> Vec<Cup> {
-    let mut cups = Vec::<Cup>::with_capacity(cards as usize);
-    cups.resize(cards as usize, Cup { value: 0, next: 0 });
+fn run_any(input: &[u8], cards: u32, games: u32) -> Vec<u32> {
+    let mut cups = Vec::<u32>::with_capacity(cards as usize);
+    cups.resize(cards as usize, 0);
 
     let mut prev = -1;
     for value in input
@@ -48,42 +41,31 @@ fn run_any(input: &[u8], cards: u32, games: u32) -> Vec<Cup> {
         .map(|c| u32::from(c - b'0'))
         .chain(10..cards)
     {
-        let cup = Cup { value, next: 0 };
-        cups[value as usize] = cup;
+        cups[value as usize] = 0;
 
         if prev >= 0 {
-            cups[prev as usize].next = value;
+            cups[prev as usize] = value;
         }
         prev = value as i32;
     }
 
     let mut current = usize::from(input[0] - b'0') as u32;
-    cups[prev as usize].next = current;
+    cups[prev as usize] = current;
 
     #[cfg(debug_assertions)]
     {
-        for (i, cup) in cups.iter().enumerate().skip(1) {
-            debug_assert!(
-                cup.next != 0,
-                "Cup {} at index {} is not linked",
-                cup.value,
-                i
-            );
+        for (i, &cup) in cups.iter().enumerate().skip(1) {
+            debug_assert!(cup != 0, "Cup {} is not linked", i);
         }
     }
 
     let limit = cards - 1;
     for _ in 0..games {
-        let cup = cups[current as usize];
-        let pickup1 = cups[cup.next as usize];
-        let pickup2 = cups[pickup1.next as usize];
-        let pickup3 = cups[pickup2.next as usize];
+        let c1 = cups[current as usize];
+        let c2 = cups[c1 as usize];
+        let c3 = cups[c2 as usize];
 
-        let c1 = pickup1.value;
-        let c2 = pickup2.value;
-        let c3 = pickup3.value;
-
-        let mut target = cup.value - 1;
+        let mut target = current - 1;
         while target == 0 || target == c1 || target == c2 || target == c3 {
             if target == 0 {
                 target = limit;
@@ -92,13 +74,9 @@ fn run_any(input: &[u8], cards: u32, games: u32) -> Vec<Cup> {
             }
         }
 
-        let target = &mut cups[target as usize];
-        let after_target = target.next;
-        target.next = pickup1.value;
-        cups[c3 as usize].next = after_target;
-
-        let after_current = pickup3.next;
-        cups[current as usize].next = after_current;
+        let after_target = std::mem::replace(&mut cups[target as usize], c1);
+        let after_current = std::mem::replace(&mut cups[c3 as usize], after_target);
+        cups[current as usize] = after_current;
         current = after_current;
     }
 
